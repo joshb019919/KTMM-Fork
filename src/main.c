0f6c3a0fca9ae515941dbb5927f110a68dc56e98
Jared Draper <jared@freshlycutwax.com>
2025-03-01 22:41:26 -0600

    refactored init and exit functions

--- src/main.c
+++ src/main.c
@@ -5,13 +5,15 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("CSC450 Group 4");
 MODULE_DESCRIPTION("Tiered memory module.");
 
-int init_module(void) {
+static int __init tmem_init(void) {
 	printk(KERN_INFO "Hello world!\n");
 	return 0;
 }
 
-void cleanup_module(void)
+static int __exit tmem_exit(void)
 {
 	printk(KERN_INFO "Goodbye world!\n");
 }
 
+module_init(tmem_init);
+module_exit(tmem_exit);
