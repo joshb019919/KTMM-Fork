6167a043b3c92eda4de6bf58d4f259185b62af20
Jared Draper <jared@freshlycutwax.com>
2025-03-09 01:27:26 -0600

    scan_node now has access to the lruvec

--- src/tmemscan.c
+++ src/tmemscan.c
@@ -4,7 +4,9 @@
  *  Page scanning and related functions for tmem module.
  *
  */
+//#include <linux/kallsyms.h>
 #include <linux/kernel.h>
+#include <linux/kprobes.h>
 #include <linux/module.h>
 #include <linux/memcontrol.h>
 #include <linux/mmzone.h>
@@ -17,27 +19,67 @@
 // static struct scan_control = { }
 
 
+static struct kprobe kp = {
+	.symbol_name = "kallsyms_lookup_name"
+};
+
+
 // (pg_data_t *pgdat, struct scan_control tsc)
-static void scan_node(pg_data_t *pgdat)
+/*
+ * This is really hacky, but we had no other choice.
+ */
+static void scan_node(pg_data_t *pgdat, int nid)
 {
-	enum lru_list lru;
+	//enum lru_list lru;
 	struct mem_cgroup *memcg;
 	struct mem_cgroup *root;
 	struct lruvec *lruvec;
+	unsigned long cgroup_iter_addr;
 	unsigned long node_lru_folios;
 
-	root = NULL;
-	memcg = mem_cgroup_iter(root, NULL, NULL);
+	typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+	kallsyms_lookup_name_t kallsyms_lookup_name;
+	register_kprobe(&kp);
+	kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+	unregister_kprobe(&kp);
 
-	lruvec = mem_cgroup_lruvec(memcg, pgdat);
 
-	node_lru_folios = 0;
+	cgroup_iter_addr = kallsyms_lookup_name("mem_cgroup_iter");
 
-	for_each_evictable_lru(lru) 
+	if(cgroup_iter_addr)
 	{
-		pr_info("Scanning evictable LRU list: %d\n", lru);
-		// call list_scan function
+		pr_info("mem_cgroup_iter addr: %lu\n", cgroup_iter_addr);
+
+		// sorry, this is a pile of shit
+		struct mem_cgroup *(*cgroup_iter_fn)(struct mem_cgroup *root, 
+			struct mem_cgroup *prev,
+			struct mem_cgroup_reclaim_cookie *reclaim)
+			= (struct mem_cgroup *(*)(struct mem_cgroup *, 
+				struct mem_cgroup *, 
+				struct mem_cgroup_reclaim_cookie *)
+				)cgroup_iter_addr;
+
+		root = NULL;
+
+		memcg = cgroup_iter_fn(root, NULL, NULL);
+
+		node_lru_folios = 0;
+
+		// finally, get some!!
+		lruvec = &memcg->nodeinfo[nid]->lruvec;
+
+		pr_info("Lruvec Populated Zones: %d\n", lruvec->pgdat->nr_zones);
+		
+		/*
+		for_each_evictable_lru(lru) 
+		{
+			pr_info("Scanning evictable LRU list: %d\n", lru);
+			// call list_scan function
+		}
+		*/
 	}
+	else
+		pr_info("tmem unable to get LRU lists");
 }
 
 
@@ -51,7 +93,8 @@ void avail_nodes(void)
 	for_each_online_node(nid)
 	{
 		pg_data_t *pgdat = NODE_DATA(nid);
-		scan_node(pgdat);
+		pr_info("Node Populated Zones: %d\n", pgdat->nr_zones);
+		scan_node(pgdat, nid);
 	}
 	//return num_online_nodes();
 }
